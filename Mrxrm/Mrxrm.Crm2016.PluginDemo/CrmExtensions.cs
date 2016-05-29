d8b00f8fe325e1657c8f14d4a2e006b088330e82
Kelvin Shen <kelvinshen@outlook.com>
2016-05-29 21:32:42 +1200

    Crm entity merging.

---
+++ Mrxrm/Mrxrm.Crm2016.PluginDemo/CrmExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xrm.Client;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mrxrm.Crm2016.PluginDemo
+{
+    public static class CrmExtensions
+    {
+        /// <summary>
+        /// Sample usage: Use this method to merge a pre-image entity to a target entity
+        /// in order to produce a post-image equivalent.
+        /// </summary>
+        /// <param name="baseEntity"></param>
+        /// <param name="deltaEntity"></param>
+        /// <returns></returns>
+        public static Entity MergeWith(this Entity baseEntity, Entity deltaEntity)
+        {
+            Entity baseClone = baseEntity.Clone(false); // Don't clone related entities.
+
+            // The delta entity should only include attribute changed.
+            foreach (var attr in deltaEntity.Attributes)
+            {
+                var attrKey = attr.Key;
+                baseClone.Attributes[attrKey] = attr.Value;
+            }
+
+            return baseClone;
+        }
+    }
+}
