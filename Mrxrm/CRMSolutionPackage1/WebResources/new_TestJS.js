d8efa98397c0d27e5a5a4357482932ccf3096f66
Kelvin Shen <kelvinshen@outlook.com>
2016-06-01 14:26:03 +1200

    Demo JS namespace.

--- Mrxrm/CRMSolutionPackage1/WebResources/new_TestJS.js
+++ Mrxrm/CRMSolutionPackage1/WebResources/new_TestJS.js
@@ -1,1 +1,21 @@
 console.log();
+
+if (typeof(mrxrm) == typeof(undefined) || !mrxrm)
+    mrxrm = { __namespace: true };
+
+if (typeof (mrxrm.common) == typeof (undefined) || !mrxrm.common)
+{
+    mrxrm.Common = function () { }; // A Class
+    mrxrm.sendEmail = function () { // A static method. See the case difference?
+        // Send email
+    }
+}
+
+mrxrm.Common.prototype.add = function (num1, num2) {
+        return num1 + num2;
+};
+
+var commonObj = new mrxrm.Common();
+var result = commonObj.add(2, 3);
+
+console.log(result);
