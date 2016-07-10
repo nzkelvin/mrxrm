62b1c7ec1b6a94c9041f73cf9588d31080a7523d
Kelvin Shen <kelvinshen@outlook.com>
2016-07-10 22:22:10 +1200

    Mrxrm Katas project.

---
+++ Mrxrm/Mrxrm.Crm2016.Katas/Mrxrm.Crm2016.Katas.Customisations/scripts/Mrxrm.Account.Forms.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="C:\Users\kelvi_000\Source\Repos\mrxrm\Mrxrm\Mrxrm.Crm2016.Katas\Mrxrm.Crm2016.Katas.Customisations\intellisense/Xrm.js" />
+
+if (typeof(Mrxrm) == typeof(undefined) || !Mrxrm)
+    Mrxrm = { __namespace: true };
+
+if (typeof (Mrxrm.Account) == typeof (undefined) || !Mrxrm.Account)
+    Mrxrm.Account = { __namespace: true };
+
+Mrxrm.Account.Forms = (function () {
+    //Private properties & methods
+
+    //var x = 0;
+    //var _privateMethod = function() {
+    //    return x++;
+    //};
+
+    //Public 
+    return {
+        OnLoad: function () {
+            alert("OnLoad");
+            //TODO: Do stuff
+            //return x;
+        }
+    };
+})();
