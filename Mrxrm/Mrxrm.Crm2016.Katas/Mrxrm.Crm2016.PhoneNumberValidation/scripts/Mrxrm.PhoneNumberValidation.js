2cee4226349fe92971ab82267f7b80fd31f9a18e
Kelvin Shen <kelvinshen@outlook.com>
2016-08-01 10:03:51 +1200

    Phone number validation project started.

---
+++ Mrxrm/Mrxrm.Crm2016.Katas/Mrxrm.Crm2016.PhoneNumberValidation/scripts/Mrxrm.PhoneNumberValidation.js
@@ -0,0 +1,67 @@
+﻿if (typeof (Mrxrm) == typeof (undefined) || !Mrxrm)
+    Mrxrm = { __namespace: true };
+
+Mrxrm.PhoneNumberValidation = (function () {
+    //Private properties & methods
+
+    //var x = 0;
+    //var _privateMethod = function() {
+    //    return x++;
+    //};
+
+    //Public 
+    return {
+        OnFormLoad: function () {
+            var phoneNumbers = [
+          { formattedNumber: '+64 4 5287000' },
+          { formattedNumber: '04 5287000' },
+          { formattedNumber: '5287000' },
+            ];
+
+            var keyPressFcn = function (ext) {
+                try {
+                    var userInput = Xrm.Page.getControl("name").getValue();
+                    resultSet = {
+                        results: new Array(),
+                        commands: {
+                            id: "sp_commands",
+                            label: "Phone number format: +99 9 9999999",
+                            action: function () {
+                                // Specify what you want to do when the user
+                                // clicks the "Learn More" link at the bottom
+                                // of the auto-completion list.
+                                // For this sample, we are just opening a page
+                                // that provides information on working with
+                                // accounts in CRM.
+                                //window.open("http://www.microsoft.com/en-us/dynamics/crm-customer-center/create-or-edit-an-account.aspx");
+                            }
+                        }
+                    };
+
+                    var userInputLowerCase = userInput.toLowerCase();
+                    for (i = 0; i < phoneNumbers.length; i++) {
+                        // todo: calculate phone number in different format here.                        
+                        resultSet.results.push({
+                            id: i,
+                            fields: [phoneNumbers[i].formattedNumber]
+                        });
+                        
+                        if (resultSet.results.length >= 10) break;
+                    }
+
+                    if (resultSet.results.length > 0) {
+                        ext.getEventSource().showAutoComplete(resultSet);
+                    } else {
+                        ext.getEventSource().hideAutoComplete();
+                    }
+                } catch (e) {
+                    // Handle any exceptions. In the sample code,
+                    // we are just displaying the exception, if any.
+                    console.log(e);
+                }
+            };
+
+            Xrm.Page.getControl("name").addOnKeyPress(keyPressFcn);    
+        }
+    };
+})();
