a5d22da2307e6f5321810558fc7099a0aa21a8c6
Kelvin Shen <kelvinshen@outlook.com>
2016-08-23 09:07:28 +1200

    Mrxrm.Common

---
+++ Mrxrm/Mrxrm.Crm2016.Katas/Mrxrm.Crm2016.Katas.Customisations/scripts/Mrxrm.Common.js
@@ -0,0 +1,21 @@
+﻿var Mrxrm = (function () {
+    //Private properties & methods
+
+    //var x = 0;
+    //var _privateMethod = function() {
+    //    return x++;
+    //};
+
+    //Public 
+    return {
+        function1: function () {
+            //TODO: Do stuff
+            //return x;
+        },
+        pad(num, size) {
+            var s = num+"";
+            while (s.length < size) s = "0" + s;
+            return s;
+        }
+    };
+})();
