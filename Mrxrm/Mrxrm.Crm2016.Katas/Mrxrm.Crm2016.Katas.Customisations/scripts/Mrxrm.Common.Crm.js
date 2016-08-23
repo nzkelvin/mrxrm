d6a195f21793a6c872f8d5cc4cb19c10428c5448
Kelvin Shen <kelvinshen@outlook.com>
2016-08-24 08:55:22 +1200

    mrxrm.common.crm.js

---
+++ Mrxrm/Mrxrm.Crm2016.Katas/Mrxrm.Crm2016.Katas.Customisations/scripts/Mrxrm.Common.Crm.js
@@ -0,0 +1,27 @@
+﻿if (typeof(Mrxrm) == typeof(undefined)) Mrxrm = { __namespace: true };
+
+if (typeof(Mrxrm.Common) == typeof(undefined)) Mrxrm.Common = { __namespace: true };
+
+Mrxrm.Common.Crm = (function () {
+    //Private properties & methods
+
+    //var x = 0;
+    //var _privateMethod = function() {
+    //    return x++;
+    //};
+
+    //Public 
+    return {
+        addFilter: function() {
+            var customerAccountFilter = "<filter type='and'><condition attribute='accountid' operator='null' /></filter>";
+            Xrm.Page.getControl("customerid").addCustomFilter(customerAccountFilter, "account");
+        },
+        defaultcustomer: function() {
+            Xrm.Page.getControl("customerid").addPreSearch(Mrxrm.Common.Crm.addFilter);
+        },
+        setDefaultCustomerLookup: function () {
+            Xrm.Page.getControl("customerid").setDefaultView("{00000000-0000-0000-00AA-000010001004}");
+            //Xrm.Page.getControl("customerid").setDefaultView("{00000000-0000-0000-00AA-000010001003}");
+        }
+    };
+})();
