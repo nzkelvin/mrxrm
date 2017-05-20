499fb95a98b4359663f487be3cf5a751e2919f50
kelvin.shen <kelvin.shen@fusion5.co.nz>
2017-05-20 21:12:59 +1200

    Created workflow distributor project.

---
+++ Mrxrm.D365/Mrxrm.D365.WorkflowTools/Mrxrm.D365.WorkflowTools/IDistributeWorkflowActivity.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+
+namespace Mrxrm.D365.WorkflowTools.Mrxrm.D365.WorkflowTools
+{
+    public interface IDistributeWorkflowActivity
+    {
+        IEnumerable<Guid> GetKeys(IOrganizationService orgService);
+    }
+
+    public static class DistributeWorkflowActivityExtensions
+    {
+        public static void Distribute(this IDistributeWorkflowActivity activity, Guid workflowId, IOrganizationService orgService, ITracingService tracer)
+        {
+            var keys = activity.GetKeys(orgService);
+
+            foreach (var key in keys)
+            {
+                ExecuteWorkflowRequest req = new ExecuteWorkflowRequest()
+                {
+                    EntityId = key,
+                    WorkflowId = workflowId
+                };
+
+                var resp = (ExecuteWorkflowResponse)orgService.Execute(req);
+                tracer.Trace($"A system job {resp.Id} is created for the workflow {workflowId}");
+            }
+        }
+    }
+}
